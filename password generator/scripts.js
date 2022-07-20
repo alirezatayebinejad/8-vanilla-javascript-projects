043fa6d9231b9bc1e0252dc81e85017620078a04
vibracode <codevibra@gmail.com>
2022-07-21 04:22:08 +0430

    some js work - pass generator

--- password generator/scripts.js
+++ password generator/scripts.js
@@ -1,1 +1,37 @@
 "use strict";
+
+let generateBox = document.querySelector("#generated-pass");
+let copyPass = document.querySelector("#copy");
+let passLengthBox = document.querySelector("#pass-length-box");
+let passLengthRange = document.querySelector("#pass-length-range");
+let uppercaseCheck = document.querySelector("#uppercase");
+let lowercaseCheck = document.querySelector("#lowercase");
+let numbersCheck = document.querySelector("#numbers");
+let symbolsCheck = document.querySelector("#symbols");
+let btnGenerate = document.querySelector("#generat-btn");
+
+let passLength = passLengthBox.value;
+let alphabets = [["a", "b"]];
+
+passLengthRange.addEventListener("change", () => {
+    passLengthBox.value = passLengthRange.value;
+    passLength = passLengthBox.value;
+});
+
+let generatePass = () => {
+    console.log("here");
+};
+
+btnGenerate.addEventListener("click", () => {
+    if (
+        !uppercaseCheck.checked &&
+        !lowercaseCheck.checked &&
+        !numbersCheck.checked &&
+        !symbolsCheck.checked
+    ) {
+        document.getElementById("warning").style.display = "block";
+    } else {
+        document.getElementById("warning").style.display = "none";
+        generatePass();
+    }
+});
