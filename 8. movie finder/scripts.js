64d9348eb53a975f5e7bdc9d5538e0f70e3bb2c4
vibracode <codevibra@gmail.com>
2022-08-03 15:55:18 +0430

    changing design

---
+++ 8. movie finder/scripts.js
@@ -0,0 +1,20 @@
+"use strict";
+
+fetchDataFromApi();
+function fetchDataFromApi() {
+	/*
+	fetch(`https://imdb-api.com/en/API/Search/k_xo3qd1dc/inception`)
+		.then((res) => {
+			if (res.status === 200) {
+				return res.json();
+			}
+			return new Error("Error:(");
+		})
+		.then((data) => {
+			console.log("data: ", data);
+		})
+		.catch((err) => {
+			console.log("error", err);
+		});
+	*/
+}
